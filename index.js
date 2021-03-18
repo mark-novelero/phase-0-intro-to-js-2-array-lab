51e84ef539173ebecf0e4ec6d6d49295934bcd9b
mark novelero <mark.novelero@gmail.com>
2021-03-18 16:42:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function beforeEach(cats){
+    cats.length = 0; 
+    cats.push("Milo", "Otis", "Garfield");
+
+    return cats;
+}
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop(0);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(0);
+}
+
+function appendCat(name){
+    var copyCat= [...cats, name];
+    return copyCat;
+}
+
+function prependCat(name = 'Arnold'){
+    var newCats = cats.slice();
+    newCats.unshift(name);
+    return newCats; 
+}
+
+function removeLastCat(){
+    var copiedCats = cats.slice();
+    copiedCats.pop(0);
+    return copiedCats;
+}
+
+function removeFirstCat(){
+    var cloneCats = cats.slice(); 
+    cloneCats.shift(0);
+    return cloneCats; 
+}
